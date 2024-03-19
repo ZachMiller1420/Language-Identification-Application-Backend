1a7559e503a20ba0171f2715e932e769b394880c
Zach Miller <zachmiller697@outlook.com>
2024-03-18 23:02:07 -0500

    Added predict to model

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
+CORS(app)
 
-MODEL_PATH = 'model.pckl'
-
-with open(MODEL_PATH, 'rb') as f:
-    model = pickle.load(f)
+# Load your machine learning model
+global Lrdetect_Model
+with open('model.pckl', 'rb') as model_file:
+    Lrdetect_Model = pickle.load(model_file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Get data from the request
     data = request.get_json()
     text = data['text']
+    
     # Perform prediction using your machine learning model
-    # prediction = model.predict(text)
-    # Replace 'Your prediction here' with the actual prediction
-    return jsonify({'prediction': 'Your prediction here'})
+    prediction = Lrdetect_Model.predict([text])[0]
+    
+    # Return prediction as JSON
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
